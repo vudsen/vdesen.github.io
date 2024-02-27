816bdef63fc53c9646dc2c85ec056fb1108e2989
icebing <xu2237803016@outlook.com>
2024-02-27 17:54:59 +0800

    fix xml bug

--- scripts/xml-adapter.js
+++
@@ -1,6 +0,0 @@
-hexo.extend.filter.register('after_post_render', function(data){
-    if (data.content && data.content.replaceAll) {
-        data.content = data.content.replaceAll('&lt;', '&amp;lt;')
-        data.content = data.content.replaceAll('&gt;', '&amp;gt;')
-    }
-})
